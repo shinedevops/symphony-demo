a438ef4a48c77cb42602936d7fe1eba61fdb5572
sachin-dev <sachin.gupta@shinedezign.com>
2025-11-20 19:07:38 +0530

    updates

--- src/Controller/Front/BlogController.php
+++ src/Controller/Front/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Front;
 use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
@@ -12,8 +13,17 @@ class BlogController extends AbstractController
     #[Route('/blog/{slug}', name: 'blog_show', methods: ['GET'])]
     public function show(Article $article): Response
     {
+        
         return $this->render('front/blog/show.html.twig', [
             'article' => $article,
+        ]);
+    }
+    #[Route('/', name: 'blog_home', methods: ['GET'])]
+    public function home(ArticleRepository $articleRepository): Response
+    {
+          $articles = $articleRepository->findAll();
+         return $this->render('front/blog/home.html.twig', [
+            'articles' => $articles,
         ]);
     }
 }
